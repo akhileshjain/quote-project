edd8fc003bde4b392d4e77ebdc8936186fab709e
Akhilesh Jain <akhilesh.taurus@gmail.com>
2021-05-07 22:54:33 +0530

    complete project

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,13 +1,22 @@
-import { useRef } from 'react';
-
-import Card from '../ui/Card';
-import LoadingSpinner from '../ui/LoadingSpinner';
+import { Fragment, useRef, useState } from 'react';
+import {Prompt} from 'react-router-dom';
+import Card from '../UI/Card';
+import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
 
 const QuoteForm = (props) => {
+
+  const [isEntering, setIsEntering] = useState(false);
+
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
+  function formFocusHandler() {
+    setIsEntering(true);
+  }
+  function finishedEnteringHandler() {
+    setIsEntering(false);
+  }
   function submitFormHandler(event) {
     event.preventDefault();
 
@@ -20,27 +29,33 @@ const QuoteForm = (props) => {
   }
 
   return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+    <Fragment>
+      <Prompt when={isEntering} message={(location) => 
+      `Are you sure you want to leave this page? Changes will be lost `} ></Prompt>
+      <Card>
+        <form 
+        onFocus={formFocusHandler}
+        className={classes.form} onSubmit={submitFormHandler}>
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
+          <div className={classes.control}>
+            <label htmlFor='author'>Author</label>
+            <input type='text' id='author' ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='text'>Text</label>
+            <textarea id='text' rows='5' ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={finishedEnteringHandler} className='btn'>Add Quote</button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
